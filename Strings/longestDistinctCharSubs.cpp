5a5c79b13a4cfb8305fff7102962925735d1d666
Saksham Checker <checkersaksham@yahoo.com>
2020-12-19 18:10:44 +0530

    Longest Distant character SubLongest Distant character Substring; endof Stringsstring; endof Strings

--- Strings/longestDistinctCharSubs.cpp
+++ Strings/longestDistinctCharSubs.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 using namespace std;
+#include <vector>
+
 const int cha=256;
-int getLongest(string s1){
+int getLongest(string s1){ //O(n2)
 	int ma=0;
 	for(int j=0;j<s1.length();j++){
 		int temp=0;
@@ -16,6 +18,19 @@ int getLongest(string s1){
 		ma=max(ma,temp);
 	}
 	return ma;
+}
+int getLongest1(string s1){ //O(n)
+	int n=s1.length();
+	int res=0;
+	vector<int> prev(256,-1);
+	int i=0;
+	for (int j = 0; j < n; ++j){
+		i=max(i,prev[s1[j]]+1);
+		int maxEnd=j-i+1;
+		res=max(res,maxEnd);
+		prev[s1[i]]==j;
+	}
+	return res;
 }
 int main(int argc, char const *argv[])
 {
