687ead3d42c6fb9fd06b0fcf581d12176df66ded
Saksham <checkersaksham@yahoo.com>
2021-01-04 17:45:40 +0530

    Addition of Index based greaters

--- Stack/nextGreaterElement.cpp
+++ Stack/nextGreaterElement.cpp
@@ -16,11 +16,30 @@ vector<int> getGreater(int a[],int n){
 	}
 	return b;
 }
+
+
+vector<int> getGreater1(int a[],int n){ //Index
+    vector<int> b(n);
+    stack<int> s;
+    b[n-1]=0;
+    s.push(n);
+    for(int i=n-2;i>=0;i--){
+        while(s.empty()==0 && a[i]>=a[s.top()-1]){
+            s.pop();
+        }
+        b[i]=s.empty()?0:s.top();
+        s.push(i+1);
+    }
+    return b;
+}
+
+
+
 int main(int argc, char const *argv[])
 {
-	int a[]={11,55,33,22,67};
+	int a[]={5,4,3,4,5};
 	vector<int> b;
-	b=getGreater(a,5);
+	b=getGreater1(a,5);
 	for(int i=0;i<5;i++)	cout<<b[i]<<' ';
 	return 0;
 }
--- Stack/previousGreaterElement.cpp
+++ Stack/previousGreaterElement.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <stack>
+#include <vector>
+
 using namespace std;
 void getGreater(int a[],int n){
 	stack<int> s;
@@ -13,9 +15,30 @@ void getGreater(int a[],int n){
 		s.push(a[i]);
 	}
 }
+
+
+vector<int> getGreater1(int a[],int n){
+	stack<int> s;
+	vector<int> b(n);
+	s.push(1);
+	b[0]=0;
+	for(int i=1;i<n;i++){
+		while(s.empty()!=1 && a[i]>=a[s.top()-1])
+			s.pop();
+		int greater=(s.empty())?0:s.top();
+		b[i]=greater;
+		s.push(i+1);
+	}
+	return b;
+}
+
+
+
 int main(int argc, char const *argv[])
 {
-	int a[]={4,3,5,2,1};
-	getGreater(a,5);
+	int a[]={5,4,3,4,5};
+	//getGreater(a,5);
+	vector<int> b=getGreater1(a,5);
+	for(int i=0;i<5;i++)	cout<<b[i]<<' ';
 	return 0;
 }
